8837bba6573f87fb84565f15230212d21d0e7dd2
Isaac Lyons <isaac@snowskeleton.net>
2025-06-29 20:22:09 -0400

    add example enviornment file

---
+++ example.env.py
@@ -0,0 +1,13 @@
+# email configuration
+SMTP_SERVER = ""
+SMTP_PORT = 587
+SMTP_USERNAME = ""
+SMTP_PASSWORD = ""
+FROM_ADDRESS = ""
+
+# General configuration
+ADMIN_EMAIL = ""  # Used for the first login to add other users
+MAGIC_LINK_EXPIRY_SECONDS = 300  # 5 minutes
+COOKIE_SECRET = "your-secret-key"  # Replace with a secure random key
+
+URI = "https://yourdomain.com"  # Replace with your actual domain
