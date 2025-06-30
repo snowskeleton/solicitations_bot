b081cb0bbb942a6eb97e059b20aba7dc89086ce9
Isaac Lyons <isaac@snowskeleton.net>
2025-06-29 20:40:54 -0400

    let it run with gunicorn

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from routes import app
 from storage import setup_db, add_user
 
 
+setup_db()
+add_user(ADMIN_EMAIL, is_admin=True)
+
 if __name__ == "__main__":
     # Suppress SSL warnings for self-signed certs
     # import urllib3
     # urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-    setup_db()
-    add_user(ADMIN_EMAIL, is_admin=True)
     app.run(host="0.0.0.0", port=5002, debug=True)
